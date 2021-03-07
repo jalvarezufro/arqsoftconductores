d834b8710c772daee8e6463f23bf857d7bfdb237
Javier <j.alvarez14@ufromail.cl>
2021-03-07 20:27:15 -0300

    funcional paraa angular

--- src/main/java/com/arquitect/arq/controler/Controler.java
+++ src/main/java/com/arquitect/arq/controler/Controler.java
@@ -4,52 +4,51 @@ import com.arquitect.arq.interfaceService.IConductorService;
 import com.arquitect.arq.model.Conductor;
 import com.arquitect.arq.service.ConductorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
-
-@Controller
+@CrossOrigin("http://200.13.4.226/")
+@RestController
 @RequestMapping
-public class Controler {
+
+public class Controler  {
+
     @Autowired
     private IConductorService service;
 
     @GetMapping("/listar")
-    public String listar(Model model) {
+    public ResponseEntity<List<Conductor>> listar(Model model) {
         List<Conductor> conductores = service.listar();
-        model.addAttribute("conductores", conductores);
-        return "index";
+
+        return new ResponseEntity<List<Conductor>>(conductores, HttpStatus.OK);
     }
 
-    @GetMapping("/new")
-    public String agregar(Model model) {
-        model.addAttribute("conductor", new Conductor());
-        return "form";
-    }
 
     @PostMapping("/save")
-    public String save(@Valid Conductor c, Model model) {
-        service.save(c);
-        return "redirect:/listar";
-    }
-    @GetMapping("/edit/{id}")
-    public String editar(@PathVariable int id, Model model){
-        Optional<Conductor> conductor = service.listarId(id);
-        model.addAttribute("conductor",conductor);
-        return "edit";
+    public Conductor save(@RequestBody Conductor c) {
+        return service.save(c);
     }
 
-    @GetMapping("/delete/{id}")
-    public String delete(Model model, @PathVariable int id){
-        service.delete(id);
-        return "redirect:/listar";
+    @GetMapping("/detalle/{id}")
+    public Optional<Conductor> editar(@PathVariable int id, Model model) {
+        Optional<Conductor> conductor = service.listarId(id);
+
+        return conductor;
     }
+
+    @RequestMapping(method = RequestMethod.DELETE, path="/delete/{id}")
+        public String delete(Model model,@PathVariable int id){
+            service.delete(id);
+            return "listo";
+        }
+
 }
